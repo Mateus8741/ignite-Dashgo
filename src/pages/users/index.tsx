9d209382dcc83bc38bcb257a21825168ebb3932e
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-02-20 22:38:06 -0300

    feat: Responsivity has been finished

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  useBreakpointValue,
 } from '@chakra-ui/react'
 
 import { Header } from '@/components/Header'
@@ -20,6 +21,11 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Pagination } from '@/components/Pagination'
 
 export default function UsersList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -27,7 +33,7 @@ export default function UsersList() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
@@ -39,44 +45,121 @@ export default function UsersList() {
               fontSize="sm"
               colorScheme="pink"
               leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              _hover={{
+                cursor: 'pointer',
+              }}
             >
               Criar novo
             </Button>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+          <Table colorScheme="whiteAlpha" w={['100%']}>
             <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
-              </Tr>
+              {isWideVersion ? (
+                <Tr>
+                  <Th px={['0', '0', '6']} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  {isWideVersion && <Th>Data de cadastro</Th>}
+                  <Th></Th>
+                </Tr>
+              ) : (
+                <Tr>
+                  <Th
+                    px={['0', '0', '6']}
+                    color="gray.300"
+                    display="flex"
+                    justifyContent="space-between"
+                  >
+                    <Text>Selecionar todos:</Text>
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                </Tr>
+              )}
             </Thead>
-
             <Tbody>
-              <Tr>
-                <Td px="6">
+              <Tr
+                display={['block', 'block', 'table-row']}
+                borderBottom={['2px dashed gray', '2px dashed gray', 'none']}
+                _last={{ borderBottom: 'none' }}
+              >
+                <Td
+                  px={['0', '0', '6']}
+                  textAlign={['right', 'right', 'inherit']}
+                  justifyContent={['space-between', 'space-between', 'inherit']}
+                  display={['flex', 'flex', 'revert']}
+                  _before={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'left',
+                    color: 'gray.400',
+                    content: `attr(data-label)`,
+                  }}
+                  data-label={!isWideVersion ? 'Check:' : ''}
+                >
                   <Checkbox colorScheme="pink" />
                 </Td>
-                <Td>
+                <Td
+                  px={['0', '0', '6']}
+                  textAlign={['right', 'right', 'inherit']}
+                  justifyContent={['space-between', 'space-between', 'inherit']}
+                  display={['flex', 'flex', 'revert']}
+                  _before={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'left',
+                    color: 'gray.400',
+                    content: `attr(data-label)`,
+                  }}
+                  data-label={!isWideVersion ? 'Usuário:' : ''}
+                >
                   <Box>
-                    <Text fontWeight="bold">Mateus Tavares</Text>
+                    <Text fontWeight="bold">Nome Usuário</Text>
                     <Text fontSize="sm" color="gray.300">
-                      test@test.com
+                      oi@usuario.dev
                     </Text>
                   </Box>
                 </Td>
-                <Td>20 de Fevereiro, 2023</Td>
-                <Td>
+                <Td
+                  px={['0', '0', '6']}
+                  textAlign={['right', 'right', 'inherit']}
+                  justifyContent={['space-between', 'space-between', 'inherit']}
+                  display={['flex', 'flex', 'revert']}
+                  _before={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'left',
+                    color: 'gray.400',
+                    content: `attr(data-label)`,
+                  }}
+                  data-label={!isWideVersion ? 'Data de Cadastro:' : ''}
+                >
+                  02 de Março, 2022
+                </Td>
+                <Td
+                  px={['0', '0', '6']}
+                  textAlign={['right', 'right', 'inherit']}
+                  justifyContent={['space-between', 'space-between', 'inherit']}
+                  display={['flex', 'flex', 'revert']}
+                  _before={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'left',
+                    color: 'gray.400',
+                    content: `attr(data-label)`,
+                  }}
+                  data-label={!isWideVersion ? 'Ações:' : ''}
+                >
                   <Button
                     as="a"
                     size="sm"
                     fontSize="sm"
                     colorScheme="purple"
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    _hover={{
+                      cursor: 'pointer',
+                    }}
                   >
                     Editar
                   </Button>
